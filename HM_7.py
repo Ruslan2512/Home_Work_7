ac86557ccd4025728c82c4cd7dacd1319a4174ec
Ruslan2512 <ruslansirenko25@gmail.com>
2023-03-04 16:51:27 +0200

    Update HM_7

--- HM_7.py
+++ HM_7.py
@@ -1,69 +1,101 @@
 from collections import UserDict
-from datetime import datetime
-import re
+import pickle
 
 
-class AddressBook(UserDict):                          #
-    def add_record(self, record):
-        self.data[record.name.value] = record
+
+class UserDict:
+    contact_dict = {}
+
+
+class Record(UserDict):
+    def input_error(self):
+        def wrapper(self, quest):
+            self.lst = ['add', 'change']
+            for i in self.quest:
+                if i in self.lst:
+                    self.quest.remove(i)
+            try:
+                self.contact_dict[self.quest[0].title()] = self.quest[1]
+            except IndexError:
+                print('Give me name and phone please')
+            except KeyError:
+                print("...")
+
+            self.set_questions()
+        return wrapper
+
+    @input_error
+    def quest_1(self, quest):
+        self.set_questions()
         
-    
-    def iterator(self, N):
-        for i in self.data:
-            while N:
-                print(i)
-                N -= 1
+    @input_error    
+    def quest_2(self, quest):
+        self.set_questions()
+                       
+    def quest_3(self, quest):        
+        if self.quest[1].casefold().title() in self.contact_dict:
+            for k, v in self.contact_dict.items():
+                if k.casefold() == self.quest[1].casefold():
+                    print(v)
+                    self.set_questions()
+                else:
+                    continue
+        else:
+            print('Enter user name')
+            self.set_questions()
+
+
+class AddressBook(Record):
+    def set_questions(self):
+        while True:
+            self.quest = input().casefold()
+            if self.quest == 'hello':
+                print('How can i help you?')
+            elif 'add' in self.quest:
+                self.quest = self.quest.split()
+                return self.quest_1(self.quest)
+            elif 'change' in self.quest:
+                self.quest = self.quest.split()
+                return self.quest_2(self.quest)
+            elif 'phone' in self.quest:
+                self.quest = self.quest.split()
+                return self.quest_3(self.quest)
+            elif self.quest == 'show all':
+                print(self.contact_dict)
+            elif self.quest == 'good bye' or self.quest == 'close' or self.quest == 'exit':
+                print('Good bye!')
+                break
+            elif self.quest == '.':
+                break
+            else:
+                print('Give me name and phone please')
                 
                 
     def search_number(self):
         lst = []
         self.search_num = int(input())
-        for k, v in record.items():
+        for k, v in contact_dict.items():
             if search_num in v:
                 lst.append(k)
                 lst.append(v)
         
         return lst
-                
-                
-    def __copy__(self):
-        copy_obj = Contacts(copy.copy(self.record))
-        return copy_obj
-        
+    
+    
+    def load(self):
+        with open('data.bin', "wb") as fh:
+            pickle.dump(contact_dict, fh)
 
-    def __deepcopy__(self, memo):
-        copy_obj = Contacts(copy.deepcopy(self.record))
-        memo[id(copy_obj)] = copy_obj
-         
-        return copy_obj
-    
-    
+
+        with open('data.bin', "rb") as fh:
+            unpacked = pickle.load(fh)
+
+
 class Field:                                 #
     def __init__(self, value):
         self.value = value
 
-
-class Name(Field):
-    pass
-    
-
-
-class Phone(Field):
-    @property
-    def set_phone(self):
-        return self.value
-    
-    
-    @set_phone.setter
-    def set_phone(self):
-        self.phone = re.findall(r'[+]*[0-9]{10,12}', self.value)
-        if self.phone:
-            return self.phone
-        else:
-            return None
-
-
-
+                
 class Birthday(Field):
     @property
     def set_birthday(self):
@@ -79,35 +111,8 @@ class Birthday(Field):
             except ValueError:
                 return None
         else:
-            return None
+            return None                
 
-
-class Record:
-    def __init__(self, name: Name, phone: Phone = None):
-        self.name = name
-        self.phones = []
-        if phone:
-            self.phones.append(phone)
-            
-    def days_to_birthday(self, day_of_birthday):
-        if day_of_birthday:
-            self.day_of_birthday = day_of_birthday.split('-') 
-            self.birthday = datetime(int(self.day_of_birthday[0]), int(self.day_of_birthday[1]), int(self.day_of_birthday[2]))
-            self.current_datetime = datetime.now()
-            return self.current_datetime.date() - self.birthday.date()
-        else:
-            return None
         
-        
-    def __copy__(self):
-        copy_obj = Contacts(copy.copy(self.name),
-                            copy.copy(self.phone))
-        return copy_obj
-        
-
-    def __deepcopy__(self, memo):
-        copy_obj = Contacts(copy.deepcopy(self.name),
-                            copy.deepcopy(self.phone))
-        memo[id(copy_obj)] = copy_obj
-         
-        return copy_obj
+c = AddressBook()
+c.set_questions()
